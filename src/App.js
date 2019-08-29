bdd71c56b32d747f133a084cd081097781c31122
Hugo Douma <hugo18044@outlook.fr>
2019-08-29 12:30:49 +0200

    updated our routing system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/pages/homepage/homepage.component';
 
 function App() {
-  return <div><HomePage /></div>;
+  return (
+  <div>
+    <Route exact path='/' component={HomePage} />
+  </div>);
 }
 
 export default App;
